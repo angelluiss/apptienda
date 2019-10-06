7c79ec56264eb22b9f7ff0eff4c5649fae14d2c4
angelluiss <oslomrp@gmail.com>
2019-10-06 16:31:13 -0400

    apptienda-django

---
+++ apps/producto/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from apps.producto.models import articulo, accesorio
+
+# Register your models here.
+admin.site.register(articulo)
+admin.site.register(accesorio)
+
+
+
---
+++ apps/retiro/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.retiro.models import retiro
+
+# Register your models here.
+admin.site.register(retiro)
+
